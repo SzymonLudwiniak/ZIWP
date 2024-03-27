a400a3befc1452bf367e926bc4930c7307b11a22
kadziewiczjakub <jakubkadziewicz@jkadziewicz.dev>
2024-03-27 13:34:01 +0100

    Improved example algorithm to accept varying number of texts

--- python/testRequest.py
+++ python/testRequest.py
@@ -8,10 +8,8 @@ _connection = Connection(config_file="X:\config\ZIWP\config.yml")
 task = Task([model], _connection)
 embeds = task.run_sent(texts)
 
-sim1 = 1 - spatial.distance.cosine(embeds[0],embeds[1])
-sim2 = 1 - spatial.distance.cosine(embeds[1],embeds[2])
-sim3 = 1 - spatial.distance.cosine(embeds[0],embeds[2])
 print("Podobienstwo semantyczne zdań:")
-print("'",texts[0],"' do '",texts[1],"'",sim1)
-print("'",texts[1],"' do '",texts[2],"'",sim2)
-print("'",texts[0],"' do '",texts[2],"'",sim3)
+for (i,text) in enumerate(texts):
+    for j in range(i+1, len(texts)):
+        sim1 = 1 - spatial.distance.cosine(embeds[i],embeds[j])
+        print("'",texts[i],"' do '",texts[j],"'",sim1)
